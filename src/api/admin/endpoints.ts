e8a093c9af601d586ad4e6ff432d2ea0d41f0d3d
zeyakarim <zeyakarim470@gmail.com>
2026-04-18 15:02:47 +0530

    [feat] handle unused code

--- src/api/admin/endpoints.ts
+++ src/api/admin/endpoints.ts
@@ -1,6 +1,3 @@
 export const ADMIN_LOGIN = '/admin/auth/login';
-export const ADMIN_REGISTER = '/admin/auth/register';
 export const ADMIN_ME = '/admin/auth/me';
-export const ADMIN_PROFILE = '/admin/users/profile';
 export const ADMIN_LOGOUT = '/admin/auth/logout';
-export const ADMIN_REFRESH_TOKEN = '/admin/auth/refresh-token';
